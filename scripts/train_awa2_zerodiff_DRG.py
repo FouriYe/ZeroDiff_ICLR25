7b97e2d1c03508004e82724347ed3b8650168276
FouriYe <843041487@qq.com>
2025-02-11 19:34:50 +0800

    Update train_awa2_zerodiff_DRG.py

--- scripts/train_awa2_zerodiff_DRG.py
+++ scripts/train_awa2_zerodiff_DRG.py
@@ -8,7 +8,7 @@ import os
 os.system('''CUDA_VISIBLE_DEVICES=5 OMP_NUM_THREADS=4 python zerodiff_DRG_train.py \
 --gzsl --encoded_noise --manualSeed 9182 --preprocessing --cuda --image_embedding res101 \
 --class_embedding att --class_embedding_norm --nepoch 300 --ngh 4096 --ndh 4096 --lambda1 10 --critic_iter 5 \
---nclass_all 50 --dataroot datasets/xlsa17/data --dataset AWA2 \
+--nclass_all 50 --dataroot YourName/datasets/xlsa17/data --dataset AWA2 \
 --noiseSize 85 --attSize 85 --resSize 2048 \
 --lr 0.0005 --classifier_lr 0.001 --gamma_recons 1.0 --freeze_dec \
 --gamma_ADV 10 --gamma_VAE 1.0 --embed_type VA \
